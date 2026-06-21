6a5c7fdf1b66e5e1ea039d0333adda6384e97ea5
hackertaker37-dot <hackertaker37@gmail.com>
2026-06-22 00:08:58 +0200

    Change BOT_TOKEN in dgd_bot_fixed.py
    Updated the BOT_TOKEN for the Telegram bot.

--- dgd_bot_fixed.py
+++ dgd_bot_fixed.py
@@ -6,7 +6,7 @@ import telebot
 from flask import Flask, jsonify
 
 # ================ الإعدادات ================
-BOT_TOKEN = "8686995713:AAHvUhE7fHLsrTHKuIFHSV2YUpiAU4I6bgw"
+BOT_TOKEN = "8686995713:AAFVS08mWY50rSHdINt-hISK5M4iGIajms8"
 API_KEY = "4886d4297bcfb669bf3b3d2d8d1c4ee2"
 BASE_URL = "http://xwdsms.org"
 CHAT_IDS = ["-1003789271722"]
